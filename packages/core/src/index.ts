bcf61525e9faff5404fff5b52c9471f1425750b1
William Lee <43682783+wlee221@users.noreply.github.com>
2021-06-15 09:53:26 -0700

    Add warn to `core` (#18)
    * Add warn
    
    * Add changeset

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,3 +1,7 @@
 export const log = (message: any, ...rest: any[]) => {
     console.log(message, ...rest);
 }
+
+export const warn = (message: any, ...rest: any[]) => {
+    console.warn(message, ...rest);
+}
